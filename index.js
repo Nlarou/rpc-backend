8b747c86501d7fe648ecdc7959d5709ee3d92786
agentfeux <40972980+agentfeux@users.noreply.github.com>
2022-07-20 21:48:43 -0400

    Adding cors

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const jsonRouter = require("express-json-rpc-router");
 const dotenv = require("dotenv").config();
+const cors = require('cors');
 const app = express();
+app.use(cors())
 app.use(express.json());
 //Replace body_parser
 app.use(express.urlencoded({ extended: false }));
